905e74305751b1fe2b19ecc0cb9fbf1f1e141d20
Niclas Darville <niclasdarville+github@gmail.com>
2012-07-07 19:23:32 +0200

    Initial commit

---
+++ _postinstall/mkplaceholders.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+"Creates placeholder objects."
+
+from wsgi import *
+
+import datetime
+import os
+
+from django.contrib.auth.models import User
+from forum.models import Category, Thread, Post
+
+from markdown import markdown as md
+from smartypants import smartypants as sp
+
+
+PATH = os.path.join(os.curdir, "_postinstall", "placeholders")
+now = datetime.datetime.now() # UTC?
+c = Category.objects.create(
+        title_plain="Discussions", title_html=title_plain)
+
+def author(line):
+    categories["AUTHOR"] = line.strip()
+
+def translator(line):
+    categories["TRANSLATOR"] = line.strip()
+
+def title(line):
+    categories["TITLE"] = line.strip()
+    t = Thread.objects.create(
+            title_plain=categories["TITLE"], title_html=sp(title_plain),
+            author=categories["AUTHOR"], category=c,
+            creation_date=now, latest_reply_date=now)
+
+def characters(line):
+    name = line.strip().lower().capitalize()    
+
+    # Test if username is taken
+    u = User.objects.create(username=name)
+    # u.avatar = "..."
+    u.set_password("password")
+    u.save()
+    characters[line.strip()] = u
+
+def speaker(speaker, content):
+    p = Post.objects.create(thread=t, author=characters[speaker],
+            content_plain=content,
+            content_html=sd(md(content, extensions="nl2br")),
+            creation_date=now)
+
+def parse_text(text):
+    speaker, content, category, characters = "", "", "", {}
+    categories = {
+                    "AUTHOR": "",
+                    "TRANSLATOR": "",
+                    "TITLE": "",
+                    "CHARACTERS": ""
+                }
+
+    for line in text:
+        if line.rstrip() in categories:
+            category = line.rstrip()
+
+        elif line.rstrip() in characters:
+            speaker = line.rstrip()
+
+        elif line == "\n":
+            if speaker:
+                speaker(speaker, content)
+                speaker = ""
+                content = ""
+            else:
+                category = ""
+
+        elif category:
+            if category == categories[0]:
+                author(line)
+            elif category == categories[1]:
+                translator(line)
+            elif category == categories[2]:
+                title(line)
+            elif category == categories[3]:
+                characters(line, speaker)
+
+        elif speaker:
+            content += line.lstrip()
+
+for root, dirs, files in os.walk(PATH):
+    for name in files:
+        if name == "text.txt":
+            with text as open(name, "r"):
+                parse_text(text)
