82b2d42b48d8aa75c4541b8310ed37ecbcaef471
Niclas Darville <niclasdarville+github@gmail.com>
2012-08-30 15:08:14 +0200

    Included import statement in conditional

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'ponyforum.settings'
-import sys
 if not 'TRAVIS' in os.environ:
+    import sys
     sys.path.append('/home/dotcloud/current/ponyforum')
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
