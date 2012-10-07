150e39795304a7c8384f5ad80ddc5346b7defee2
Niclas Darville <niclasdarville+github@gmail.com>
2012-10-07 23:32:39 +0200

    Preliminary Thread object test with other modifications

--- forum/tests.py
+++ forum/tests.py
@@ -1,8 +1,16 @@
+import datetime
+
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.utils import timezone
-from forum.models import Category
+
+from forum.models import Category, Thread
+
+c = Category()
 
 class CategoryModelTest(TestCase):
+    """Tests Category object."""
+
     def test_add_category(self):
         # Create Category object
         c = Category()
@@ -21,3 +29,18 @@ class CategoryModelTest(TestCase):
         # Check the two saved fields from before
         self.assertEquals(only_category_in_db.title_plain, title)
         self.assertEquals(only_category_in_db.title_html, title)
+
+class ThreadModelTest(TestCase):
+    """Tests Thread object."""
+
+    def test_create_thread(self):
+        # Create Thread object
+        t = Thread()
+        t.category = c
+        title = "Test Thread"
+        t.title_plain, t.title_html = title, title
+        t.creation_date, t.latest_reply_date = datetime.datetime.now(), datetime.datetime.now()
+        t.author = User.objects.get(pk=1)
+
+        # Save
+        t.save()
