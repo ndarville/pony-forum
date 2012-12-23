03f722e314d74c8c89b4a5ba27708a802cd3df1d
Niclas Darville <niclasdarville+github@gmail.com>
2012-12-23 01:25:20 +0100

    Cleaned up code

--- forum/installed_apps_order_tests.py
+++ forum/installed_apps_order_tests.py
@@ -14,29 +14,26 @@ def get_apps():
 class AppOrderTest(TestCase):
     """Ensures the apps are in the order as required by their authors."""
 
+    def setUp(self):
+        self.apps = get_apps()
+
     def test_forum(self):
         """'forum' goes at the beginning cf.
         https://github.com/ndarville/pony-forum/issues/60.
         """
-        apps = get_apps()
-
-        if 'forum' in apps:
-            self.assertEquals(apps['forum'], 0)
+        if 'forum' in self.apps:
+            self.assertEquals(self.apps['forum'], 0)
 
     def test_django_nose(self):
         """'django_nose' comes after 'South' cf.
         https://github.com/jbalogh/django-nose/blob/master/README.rst#using-with-south.
         """
-        apps = get_apps()
-
-        if 'django_nose' in apps and 'South' in apps:
-            self.assertTrue(apps['django_nose'] > apps['South'])
+        if 'django_nose' in self.apps and 'South' in self.apps:
+            self.assertTrue(self.apps['django_nose'] > self.apps['South'])
 
     def test_django_admin_bootstrapped(self):
         """'django_admin_bootstrapped comes before 'django.contrib.admin' cf.
         https://github.com/riccardo-forina/django-admin-bootstrapped/blob/master/README.md.
         """
-        apps = get_apps()
-
-        if 'django_admin_bootstrapped' and 'django.contrib.admin' in apps:
-            self.assertTrue(apps['django_admin_bootstrapped'] < apps['django.contrib.admin'])
+        if 'django_admin_bootstrapped' and 'django.contrib.admin' in self.apps:
+            self.assertTrue(self.apps['django_admin_bootstrapped'] < self.apps['django.contrib.admin'])
