1dd1a4368bdc76601f2ce24be655f7cc70af4ee3
Niclas Darville <niclasdarville+github@gmail.com>
2013-02-03 20:16:29 +0100

    Created a local equivalent of postinstall

---
+++ build_forum.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Builds an example forum with placeholder objects
+# and an admin user with username 'admin', password 'password'.
+#
+# Requires that a postgreSQL database with the name 'mydb' exists.
+set -e # stops execution on error
+python manage.py syncdb --noinput
+python manage.py schemamigration forum --initial
+python manage.py migrate forum --fake
+python _postinstall/mkadmin.py
+python _postinstall/definesite.py
+python _postinstall/mkplaceholders.py
+python manage.py runserver
