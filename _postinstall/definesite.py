a1a57c33ea9f4e728c1cd0160a3bd8febb189f9d
Niclas Darville <niclasdarville+github@gmail.com>
2013-03-01 19:35:42 +0100

    Proper scoping and treatment of environment variables cf. #103

--- _postinstall/definesite.py
+++ _postinstall/definesite.py
@@ -4,9 +4,6 @@ Capitalizes the first letter of the project name during this.
 """
 import os
 
-from django.conf import settings as project_settings
-from django.contrib.sites.models import Site
-
 
 if 'DOTCLOUD_ENVIRONMENT' in os.environ:
     import json
@@ -15,13 +12,16 @@ if 'DOTCLOUD_ENVIRONMENT' in os.environ:
     with open('/home/dotcloud/environment.json') as f:
         env = json.load(f)
         name = env['DOTCLOUD_PROJECT']
+        from django.contrib.sites.models import Site
 
         s = Site.objects.get_current()
         s.domain = unicode(env['DOTCLOUD_WWW_HTTP_URL'])
         s.name   = unicode(name.capitalize())
         s.save()
 
-elif project_settings.LOCAL_DEVELOPMENT:
+else:
+    from django.contrib.sites.models import Site
+
     s = Site.objects.get_current()
     s.domain = "127.0.0.1:8000"
     s.name   = "The Forum"
