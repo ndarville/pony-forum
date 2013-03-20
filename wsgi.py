17bbbadb1ad99746f1b94003255e0d468723d659
Niclas Darville <niclasdarville+github@gmail.com>
2013-03-20 04:04:01 +0100

    Fixes #103, i.e. the Travis env var bug

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,16 @@
 """WSGI handler for dotCloud and default set-ups."""
 import os
+import sys
+
 
 if 'DOTCLOUD_ENVIRONMENT' in os.environ:
-    import sys
     import django.core.handlers.wsgi
 
     os.environ['DJANGO_SETTINGS_MODULE'] = 'ponyforum.settings'
     sys.path.append('/home/dotcloud/current/ponyforum')
     application = django.core.handlers.wsgi.WSGIHandler()
 
-else:
+elif 'TRAVIS' in os.environ:
     from django.core.wsgi import get_wsgi_application
 
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ponyforum.settings")
     application = get_wsgi_application()
