9eee8d12df02371467cc5f0ed45a9b075392a63e
Niclas Darville <niclasdarville+github@gmail.com>
2014-05-04 16:38:34 +0200

    Reinstated sp import

--- _postinstall/mkplaceholders.py
+++ _postinstall/mkplaceholders.py
@@ -12,6 +12,7 @@ if 'DOTCLOUD_ENVIRONMENT' in os.environ:
         env = json.load(f)
 
 from markdown import markdown as md
+from smartypants import smartypants as sp
 
 from django.contrib.auth.models import User
 
